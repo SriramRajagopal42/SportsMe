88c796443ed541432de7e6928ea96a5d0521c4da
Arnav Kushwah <arnavkushwah2314@gmail.com>
2024-05-13 16:12:19 -0700

    intial login/signup commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './pages/login.js';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Login />}/>
-      </Routes>
-    </Router>
+    <div>
+
+    </div>
   );
 }
 
---
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LoginSignup.css'
+
+export const LoginSignup = () => {
+  return (
+    <div>LoginSignup</div>
+  )
+}
+
+
+export default LoginSignup
