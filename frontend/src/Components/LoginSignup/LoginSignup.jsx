11bce61d51b9fe473f667477edfc7580a8417e30
Arnav Kushwah <arnavkushwah2314@gmail.com>
2024-05-13 18:08:22 -0700

    added functionality to login and sign up buttons

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 import './LoginSignup.css'
 import password_icon from '../Assets/password.png'
 import user_icon from '../Assets/person.png'
@@ -6,17 +7,19 @@ import email_icon from '../Assets/email.png'
 
 
 export const LoginSignup = () => {
+
+  const[action, setAction] = useState("Login");
   return (
     <div className='container'>
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
+        {action === "Login"?<div></div>:<div className="input">
           <img src={user_icon} alt="" />
           <input type="text" placeholder='Username' />
-        </div>
+        </div>}
         <div className="input">
           <img src={email_icon} alt="" />
           <input type="email" placeholder='Email' />
@@ -26,10 +29,10 @@ export const LoginSignup = () => {
           <input type="password" placeholder='Password' />
         </div>
       </div>
-      <div className='forgot-password'>Forgot Password? <span>Click Here</span></div>
+      {action ==="Sign Up"?<div></div>:<div className='forgot-password'>Forgot Password? <span>Click Here</span></div>}
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
-        <div className='submit'>Login</div>
+        <div className={action=== "Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
   )
