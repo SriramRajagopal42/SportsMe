ca8d0c412a81ea84d675d4faaad48eec37785dfd
Arnav Kushwah <arnavkushwah2314@gmail.com>
2024-05-13 17:32:48 -0700

    added sign up and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
 
 function App() {
   return (
     <div>
+      <LoginSignup/>
 
     </div>
   );
--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './LoginSignup.css'
+import password_icon from '../Assets/password.png'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='container'>
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className='forgot-password'>Forgot Password? <span>Click Here</span></div>
+      <div className='submit-container'>
+        <div className='submit'>Sign Up</div>
+        <div className='submit'>Login</div>
+      </div>
+    </div>
   )
 }
 
