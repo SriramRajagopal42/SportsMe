476e1f2c837c5f8c4d96839a6ea68aaa7b884a5a
SjSav <sjsavioj@gmail.com>
2024-05-14 23:59:10 -0700

    Implemented auth and integrated mongodb

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -4,16 +4,75 @@ import './LoginSignup.css'
 import password_icon from '../Assets/password.png'
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
-
+import axios from 'axios';
 
 export const LoginSignup = () => {
 
   const[action, setAction] = useState("Login");
+  const[password, setPassword] = useState("");
+  const[email, setEmail] = useState("");
+  const[status, setStatus] = useState("");
+  async function validateUser() {
+    
+    await axios.post('http://localhost:5000/login', {
+      'email': email,
+      'password': password
+    },
+  {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+)
+    .then(response => {
+      if(response['data']['success']) {
+        setStatus("");
+        //navigate to main page
+      }
+      else {
+        setStatus(response['data']['message']);
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+
+  async function addUser() {
+    
+    await axios.post('http://localhost:5000/register', {
+      'email': email,
+      'password': password
+    },
+  {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+)
+    .then(response => {
+      if(response['data']['success']) {
+        setStatus("");
+        //navigate to main page
+      }
+      else {
+        setStatus(response['data']['message']);
+      }
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+
+
   return (
     <div className='container'>
       <div className="header">
         <div className="text">{action}</div>
         <div className="underline"></div>
+        <div className = "status">{status}</div>
       </div>
       <div className="inputs">
         {action === "Login"?<div></div>:<div className="input">
@@ -22,17 +81,17 @@ export const LoginSignup = () => {
         </div>}
         <div className="input">
           <img src={email_icon} alt="" />
-          <input type="email" placeholder='Email' />
+          <input type="email" placeholder='Email' onChange = {(e) => { setEmail(e.target.value)}} />
         </div>
         <div className="input">
           <img src={password_icon} alt="" />
-          <input type="password" placeholder='Password' />
+          <input type="password" placeholder='Password' onChange = {(e) => { setPassword(e.target.value)}} />
         </div>
       </div>
       {action ==="Sign Up"?<div></div>:<div className='forgot-password'>Forgot Password? <span>Click Here</span></div>}
       <div className='submit-container'>
-        <div className={action=== "Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+        <div className={action=== "Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up"); addUser();}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login"); validateUser();}}>Login</div>
       </div>
     </div>
   )
