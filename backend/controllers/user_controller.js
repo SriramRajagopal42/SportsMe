96109c73d02119901c796121cdc530112996bdef
SriramRajagopal42 <aham.sriram@gmail.com>
2024-05-23 19:35:37 -0700

    Added proper backend routes for login/signup, added checks to make sure password is strong and email format is correct, added JWT token support to backend

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,13 +1,39 @@
 const User = require('../models/user_model')
+const jwt = require('jsonwebtoken')
 
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 //login user
 const login_user = async (req, res) => {
-    res.json({mssg: "login user"})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 //signup user
 const signup_user = async (req, res) => {
-    res.json({mssg: "signup user"})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {login_user, signup_user}
--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,57 @@ const user_schema = new Schema({
         required: true,
     }
 })
+
+// static signup method
+user_schema.statics.signup = async function(email, password) {
+    
+    //validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password must be at least eight characters long and contain at least one capital letter, one lowercase letter, one number, and one sybmol')
+    }
+    
+    const exists = await this.findOne({email})
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = this.create({email, password: hash})
+
+    return user
+}
+
+//static login method
+user_schema.statics.login = async function(email, password) {
+    //validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({email})
+
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model("User", user_schema)
