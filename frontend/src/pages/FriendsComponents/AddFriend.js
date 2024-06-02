474a2aca91751593ffe38aae83bc9c2ae229eadb
SjSav <sjsavioj@gmail.com>
2024-06-02 01:58:50 -0700

    Searching page for friends now dynamically shows all users filtered out by username typed out by user.

--- frontend/src/pages/FriendsComponents/AddFriend.js
+++ frontend/src/pages/FriendsComponents/AddFriend.js
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const AddFriend = ({ userId }) => {
   const [friendId, setFriendId] = useState('');
+  const [filtered_users, setFilteredUsers] = useState([]);
+  const {user} = useAuthContext();
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(()=> {
+    setFilteredUsers(allUsers.filter((user) => {
+          return user.username.includes(friendId);
+      }));
+
+        
+  }, [friendId]);
+
+  useEffect(() => {
+
+    const get_users = async() => {
+      try {
+          const users =  await axios.get('http://localhost:4000/api/user', {
+            headers: {
+              Authorization: `Bearer ${user.token}`
+            }
+          });
+          setAllUsers(users.data);
+          setFilteredUsers(users.data);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    get_users();
+
+  }, [user])
 
   const handleAddFriend = async () => {
     try {
-      await axios.post('/api/add-friend', { userId, friendId });
+      await axios.post('http://localhost:4000/api/user', { userId, friendId });
       alert('Friend added successfully');
     } catch (err) {
       console.error('Error adding friend', err);
@@ -22,7 +53,15 @@ const AddFriend = ({ userId }) => {
         value={friendId}
         onChange={e => setFriendId(e.target.value)}
       />
-      <button onClick={handleAddFriend}>Add Friend</button>
+      
+      {filtered_users.map((filtered_user, index)=> {
+        return (
+          <div key={index}>
+              {filtered_user.username}
+              <button onClick={handleAddFriend}>Add Friend</button>
+          </div>
+        );
+      })}
     </div>
   );
 };
